5f1de56292e8bb1b2ba9b86a0c07e5a0c07ce99b
annie <anialaverdyan2@gmail.com>
2021-09-13 11:47:57 +0400

    Without dup2

--- HW1/main.cpp
+++ HW1/main.cpp
@@ -3,21 +3,21 @@
 #include <iostream>
 int main()
 {
+  if (close(STDOUT_FILENO) < 0)
+  {
+    perror("[close]");
+    return 1;
+  }
   int fd = open("file.txt", O_WRONLY | O_CREAT, 0666);
   if (fd < 0)
   {
     perror("[open]");
     return 1;
   }
-  if (dup2(fd, STDOUT_FILENO) < 0)
-  {
-    perror("[dup2]");
-    return 1;
-  }
   std::cout << "hello world" << std::endl;
   if (close(fd) < 0)
   {
-    perror("[HW1]");
+    perror("[close]");
     return 1;
   }
   return 0;
